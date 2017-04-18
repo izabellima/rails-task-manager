e9aab1bf263e8a7240c778b5245c3e5166209ac4
Izabel Lima <belica18@gmail.com>
2017-04-18 13:14:13 +0200

    finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+task = Task.create([ {name: "laundy", status: false}, {name: "groceries", status: true}, {name: "cleaning", status: false}])
